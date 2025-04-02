eeaef66cee22dae9a92bf011461812a1114dccae
thigovenc <thiagovenceslau1997@gmail.com>
2025-04-02 14:23:14 -0300

    feat: fetchBooks and useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 import axios from 'axios';
 
 function App() {
   const [books, setBooks] = useState([]);
+
+  const fetchBooks = async () => {
+    const response = await axios.get('http://localhost:3001/books');
+    setBooks(response.data)
+  }
+  
+  useEffect(()=> {  
+    fetchBooks()
+  }, []);
 
   const editBookById = (id, newTitle) => {
     const updatedBooks = books.map((book) => {
