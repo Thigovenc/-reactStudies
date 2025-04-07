dd247da771671fae2bda678242b8e620b86d081c
thigovenc <thiagovenceslau1997@gmail.com>
2025-04-07 13:51:54 -0300

    feat: add use-books-context hook and add to the pages

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,10 +1,9 @@
-import { useContext } from 'react';
 import BookShow from './BookShow';
-import BooksContext from '../context/books';
+import useBooksContext from '../hooks/use-books-context';
 
 function BookList() {
 
-  const { books } = useContext(BooksContext)
+  const { books } = useBooksContext()
 
   const renderedBooks = books.map((book) => {
     return (
