eb8f54ec24b6cb0a596af982c326960ed3308761
thigovenc <thiagovenceslau1997@gmail.com>
2025-04-07 11:38:15 -0300

    refactor: add bookcontext in booklist page

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,21 +2,17 @@ import { useContext } from 'react';
 import BookShow from './BookShow';
 import BooksContext from '../context/books';
 
-function BookList({ books, onDelete, onEdit }) {
+function BookList() {
 
-  const { count, incrementCount } = useContext(BooksContext)
+  const { books } = useContext(BooksContext)
 
   const renderedBooks = books.map((book) => {
     return (
-      <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />
+      <BookShow key={book.id} book={book} />
     );
   });
 
   return (<div className="book-list">
-    {count}
-    <button onClick={incrementCount}>
-      click
-    </button>
     {renderedBooks}
   </div>)
 }
